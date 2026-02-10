4219247e2f23daa1a8908ad9268759e47a218f5a
Nehad <35563834+81joker@users.noreply.github.com>
2026-02-10 14:21:41 +0100

    refactor: streamline deploy script by consolidating Node dependencies installation and frontend asset building

--- deploy.sh
+++ deploy.sh
@@ -25,6 +25,13 @@ touch database/database.sqlite
 echo "[4/6] Installing composer dependencies (remote)"
 composer install --no-interaction --prefer-dist --no-dev --optimize-autoloader
 
+
+echo "[4.5/6] Installing Node dependencies"
+npm ci
+
+echo "[4.6/6] Building frontend assets"
+npm run build
+
 echo "[5/6] Running artisan tasks"
 php artisan migrate --force
 php artisan config:cache || true
@@ -38,13 +45,3 @@ echo "Deployed commit:"
 git --no-pager log -1 --pretty=oneline
 
 echo "The app has been built and deployed!"
-
-
-echo "[4/6] Installing composer dependencies (remote)"
-composer install --no-interaction --prefer-dist --no-dev --optimize-autoloader
-
-echo "[4.5/6] Installing Node dependencies"
-npm ci
-
-echo "[4.6/6] Building frontend assets"
-npm run build
