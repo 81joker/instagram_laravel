0971ef77a49f56a73f815c6134a0906134056541
Nehad <35563834+81joker@users.noreply.github.com>
2026-02-15 12:15:50 +0100

    add Modal component for post view in Livewire

---
+++ app/Livewire/Post/View/Modal.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire\Post\View;
+
+use Livewire\Component;
+
+class Modal extends Component
+{
+    public function render()
+    {
+        return <<<'HTML'
+        <div>
+            {{-- The Master doesn't talk, he acts. --}}
+        </div>
+        HTML;
+    }
+}
